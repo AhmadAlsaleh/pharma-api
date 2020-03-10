fb947e799fcc795dcd42a85dcd159a31a46e7bfd
Ahmad Anas Alsaleh <alsaleh.ahmad.a@gmail.com>
2020-03-10 21:42:00 +0200

    pharma

---
+++ models/Pharmacy.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+
+module.exports = (db) => {
+    const pharma = db.define('Pharmacy', {
+        id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4
+        },
+        title: Sequelize.STRING,
+        owner: Sequelize.STRING,
+        description: Sequelize.STRING,
+        place: Sequelize.STRING,
+        phone: Sequelize.STRING,
+        email: Sequelize.STRING,
+        isOpened: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
+        }
+    })
+
+    return pharma
+}
